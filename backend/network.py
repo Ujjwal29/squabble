5f903fbd1d4fc055e6fd6502175953205a15787d
nimit1428 <mnimitsavant@outlook.com>
2020-09-19 00:20:25 +0530

    backend socket working individually

---
+++ backend/network.py
@@ -0,0 +1,22 @@
+import socket
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.addr = (socket.gethostname(), 1024)
+        self.pos = self.connect()
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
